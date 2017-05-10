97f324a8078c35d7d8406a9881a65fbb6fbc51c2
Frank Warnaar <f.warnaar@viriciti.com>
2017-05-10 13:42:06 +0200

    padding & scales right

--- src/client/js/modules/Linechart.js
+++ src/client/js/modules/Linechart.js
@@ -3,10 +3,10 @@ const d3 = require('d3');
 class Linechart {
 	constructor(app) {
 		this.app = app;
-		this.settings = {
-			width: window.innerWidth > 1000 ? 1000 : window.innerWidth,
-			height: 400
-		}
+		this.settings = {};
+		this.settings.padding = 16;
+		this.settings.width = window.innerWidth > 1000 ? (1000 - 4 * this.settings.padding) / 3 * 2 : window.innerWidth - 4 * this.settings.padding;
+		this.settings.height = 400 - 4 * this.settings.padding;
 	}
 
 	init() {
@@ -28,14 +28,14 @@ class Linechart {
 		this.consumptionGroup = this.svg.append('g');
 
 		this.x = d3.time.scale()
-			.range([0, this.settings.width]);
+			.range([0, this.settings.width - 2 * this.settings.padding]);
 
 		this.yScore = d3.scale.linear()
-			.rangeRound([this.settings.height, 0]);
+			.rangeRound([this.settings.height - 2 * this.settings.padding, 0]);
 		this.yProductsSold = d3.scale.linear()
-			.rangeRound([this.settings.height, 0]);
+			.rangeRound([this.settings.height - 2 * this.settings.padding, 0]);
 		this.yConsumption = d3.scale.linear()
-			.rangeRound([this.settings.height, 0]);
+			.rangeRound([this.settings.height - 2 * this.settings.padding, 0]);
 
 		this.scorePath = createLine('score', 'yScore');
 		this.productsPath = createLine('productsSold', 'yProductsSold');
@@ -44,20 +44,16 @@ class Linechart {
 		const self = this;
 
 		function createLine(parameter, yScale) {
-			console.log(yScale);
 			return d3.svg.line()
 				.x(d => self.x(new Date(d.timestamp)))
-				.y(d => {
-					console.log(yScale);
-					return self[yScale](d[parameter])
-				});
+				.y(d => self[yScale](d[parameter]));
 		}
 
-		this.scoreLine = this.scoreGroup.append('path');
-		this.productsLine = this.productsGroup.append('path')
+		this.scoreLine = this.scoreGroup.append('path')
 			.attr('class', 'path__secondary');
-		this.consumptionLine = this.consumptionGroup.append('path')
-			.attr('class', 'path__tertiary');
+		this.consumptionLine = this.consumptionGroup.append('path');
+		this.productsLine = this.productsGroup.append('path')
+		.attr('class', 'path__tertiary');
 	}
 
 	update(ranking) {
@@ -67,9 +63,13 @@ class Linechart {
 		const data = caterer.data;
 
 		this.x.domain(d3.extent(data, d => new Date(d.timestamp)));
-		this.yScore.domain(d3.extent(data, d => d.score));
-		this.yProductsSold.domain(d3.extent(data, d => d.productsSold));
-		this.yConsumption.domain(d3.extent(data, d => d.consumption));
+		const maxScore = d3.max(data, d => d.score);
+		const maxProductsSold = d3.max(data, d => d.productsSold);
+		const maxConsumption = d3.max(data, d => d.consumption);
+
+		this.yScore.domain([0, maxScore]);
+		this.yProductsSold.domain([0, maxProductsSold]);
+		this.yConsumption.domain([0, maxConsumption]);
 
 		this.scoreLine.data(data);
 		this.scoreLine.attr('d', this.scorePath(data));
