0ce790668274e214c0b1fe4d2fba0809d37577a7
Frank Warnaar <f.warnaar@viriciti.com>
2017-05-10 11:29:00 +0200

    Update scales

--- src/client/js/modules/Linechart.js
+++ src/client/js/modules/Linechart.js
@@ -3,6 +3,10 @@ const d3 = require('d3');
 class Linechart {
 	constructor(app) {
 		this.app = app;
+		this.settings = {
+			width: 600,
+			height: 400
+		}
 	}
 
 	init() {
@@ -15,19 +19,35 @@ class Linechart {
 
 	setupChart(id) {
 		this.svg = d3.select('#line-chart').append('svg');
-
 		this.svg
-			.attr('width', '100%')
-			.attr('height', '100vh');
+			.attr('width', this.settings.width)
+			.attr('height', this.settings.height);
 
-		console.log(this.svg);
+		this.scoreGroup = this.svg.append('g');
+
+		this.x = d3.scaleTime()
+			.range([0, this.settings.width]);
+
+		this.yScore = d3.scaleLinear()
+			.rangeRound([this.settings.height, 0]);
+		// this.yConsumption = d3.scaleLinear()
+		// 	.rangeRound([height, 0]);
+		// this.yProductsSold = d3.scaleLinear()
+		// 	.rangeRound([height, 0]);
+		this.scoreLine = d3.line()
+			.x(d => x(new Date(d.timestamp)))
+			.y(d => y(new Date(d.score)));
 	}
 
 	update(ranking) {
 		const caterer = ranking.filter(caterer => {
 			return caterer.id === this.id;
 		})[0];
-		console.log(caterer);
+		const data = caterer.data;
+		console.log(data);
+
+		this.x.domain(d3.extent(data, d => new Date(d.timestamp)));
+		this.yScore.domain(d3.extent(data, d => new Date(d.score)));
 	}
 }
 
