ac19330ad4b520242f6110b89586ae8b46a7a3e0
Frank Warnaar <f.warnaar@viriciti.com>
2017-05-10 13:17:16 +0200

    Lines updating with styles

--- src/client/js/modules/Linechart.js
+++ src/client/js/modules/Linechart.js
@@ -4,7 +4,7 @@ class Linechart {
 	constructor(app) {
 		this.app = app;
 		this.settings = {
-			width: 600,
+			width: window.innerWidth > 1000 ? 1000 : window.innerWidth,
 			height: 400
 		}
 	}
@@ -23,30 +23,41 @@ class Linechart {
 			.attr('width', this.settings.width)
 			.attr('height', this.settings.height);
 
-		console.log(d3);
-
 		this.scoreGroup = this.svg.append('g');
+		this.productsGroup = this.svg.append('g');
+		this.consumptionGroup = this.svg.append('g');
 
 		this.x = d3.time.scale()
 			.range([0, this.settings.width]);
 
 		this.yScore = d3.scale.linear()
 			.rangeRound([this.settings.height, 0]);
-		// this.yConsumption = d3.scaleLinear()
-		// 	.rangeRound([height, 0]);
-		// this.yProductsSold = d3.scaleLinear()
-		// 	.rangeRound([height, 0]);
+		this.yProductsSold = d3.scale.linear()
+			.rangeRound([this.settings.height, 0]);
+		this.yConsumption = d3.scale.linear()
+			.rangeRound([this.settings.height, 0]);
 
-		this.scorePath = d3.svg.line()
-			.x(d => {
-				return this.x(new Date(d.timestamp));
-			})
-			.y(d => {
-				console.log(d.score);
-				return this.yScore(d.score);
-			});
+		this.scorePath = createLine('score', 'yScore');
+		this.productsPath = createLine('productsSold', 'yProductsSold');
+		this.consumptionPath = createLine('consumption', 'yConsumption');
+
+		const self = this;
+
+		function createLine(parameter, yScale) {
+			console.log(yScale);
+			return d3.svg.line()
+				.x(d => self.x(new Date(d.timestamp)))
+				.y(d => {
+					console.log(yScale);
+					return self[yScale](d[parameter])
+				});
+		}
 
 		this.scoreLine = this.scoreGroup.append('path');
+		this.productsLine = this.productsGroup.append('path')
+			.attr('class', 'path__secondary');
+		this.consumptionLine = this.consumptionGroup.append('path')
+			.attr('class', 'path__tertiary');
 	}
 
 	update(ranking) {
@@ -57,10 +68,17 @@ class Linechart {
 
 		this.x.domain(d3.extent(data, d => new Date(d.timestamp)));
 		this.yScore.domain(d3.extent(data, d => d.score));
+		this.yProductsSold.domain(d3.extent(data, d => d.productsSold));
+		this.yConsumption.domain(d3.extent(data, d => d.consumption));
 
 		this.scoreLine.data(data);
-
 		this.scoreLine.attr('d', this.scorePath(data));
+
+		this.productsLine.data(data);
+		this.productsLine.attr('d', this.productsPath(data));
+
+		this.consumptionLine.data(data);
+		this.consumptionLine.attr('d', this.consumptionPath(data));
 	}
 }
 
